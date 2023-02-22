f6851c740889f2986eefe2b71f7f3a9a70b0ef6b
Nicolás Perdomo <emusttm5543@gmail.com>
2023-02-22 15:56:50 -0300

    Añadiendo el carrito de compras

---
+++ controllers/cartController.js
@@ -0,0 +1,21 @@
+
+const Brand = require("../models/brand");
+const Cabinet = require("../models/cabinet");
+const CPU = require("../models/cpu");
+const GPU = require("../models/gpu");
+const Motherboard = require("../models/motherboard");
+const PowerSupply = require("../models/powerSupply");
+const RAM = require("../models/ram");
+const Storage = require("../models/storage");
+const Computer = require("../models/computer");
+
+const { body, validationResult } = require("express-validator");
+
+const async = require("async");
+const fs = require("fs");
+
+exports.index = function (req, res, next) {
+    // res.send('Está hecho el carrito nashe')
+    res.render('cart', {title:"Shopping Cart"})
+
+};
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,15 @@ var express = require('express');
 var router = express.Router();
 const computer_controller = require("../controllers/computerController");
 const index_controller = require("../controllers/indexController");
-
+const cart_controller = require("../controllers/cartController");
 // Middleware of multers 
 const upload = require("../middlewares/uploadImage");
 
 /* GET home page. */
 router.get('/', index_controller.index);
 
+router.get('/cart', cart_controller.index);
+// router.post('/cart', cart_controller.buy);
 
 router.get("/pcBuild", computer_controller.computer_create_get);
 router.post("/pcBuild", upload.single('image'), computer_controller.computer_create_post);
